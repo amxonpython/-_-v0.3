97e1e7e2a3630109cad1108b485892fa1a0f8209
lampa <134073971+amxonpython@users.noreply.github.com>
2026-05-06 13:37:07 +0300

    Create comands.java

---
+++ comands.java
@@ -0,0 +1,11 @@
+public class comands {
+    public static void help(){
+        System.out.println("доступные команды:");
+        System.out.println("добавить товар");
+        System.out.println("добавить покупку");
+        System.out.println("удалить товар");
+        System.out.println("список товара");
+        System.out.println("exit - выход");
+        System.out.println();
+    }
+}
