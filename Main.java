2e6e93cd8d180834e15796e2cdc2529cfb2b2275
lampa <134073971+amxonpython@users.noreply.github.com>
2026-05-06 13:35:42 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,18 +5,16 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> id_tavara_list = new ArrayList<>();
+        List<String> name_tavara_list = new ArrayList<>();
+        List<Integer> price_tavara_list = new ArrayList<>();
 
-        int i = 0;
+        List<Boolean> deleted_product_list = new ArrayList<>();
+
+        int i = -1;
+
         int sales_counter = 0;
 
-        int max_massiv = 10000000;
-
-        String[] name_tavara = new String[max_massiv];
-        int[] id_tavara = new int[max_massiv];
-        int[] price_tavara = new int[max_massiv];
-
-        int[] deletes = new int[max_massiv];
-        boolean[] deleted_product = new boolean[max_massiv];
 
         boolean working = true;
 
@@ -24,37 +22,42 @@ public class Main {
             System.out.println("выберите действие: ");
             String action = scanner.nextLine();
             switch (action){
+
                 case "":
                     break;
+
                 case "добавить товар":
-                    if(i == max_massiv){
-                        System.out.println("вы дастигнули лимит товаров");
-                    }else {
-                        i++;
-                        System.out.println("введите название: ");
-                        String name = scanner.nextLine();
+                    i++;
+                    System.out.println("введите название: ");
+                    String name = scanner.nextLine();
+                    System.out.println("введите цену");
+                    int price = scanner.nextInt();
 
-                        System.out.println("введите цену");
-                        int price = scanner.nextInt();
+                    price_tavara_list.add(i, price);
+                    name_tavara_list.add(i, name);
+                    id_tavara_list.add(i, i);
 
-                        price_tavara[i] = price;
-                        name_tavara[i] = name;
-                        id_tavara[i] = i;
-                        System.out.println("создан товар с id: " + i + " названием: " + name_tavara[i] + " и ценой: " + price_tavara[i]);
-                        scanner.nextLine();
-                        break;
-                    }
+                    deleted_product_list.add(false);
+
+                    System.out.println("создан товар с id: " + id_tavara_list.get(i) + " названием: " + name_tavara_list.get(i) + " и ценой: " + price_tavara_list.get(i));
+                    scanner.nextLine();
+
+                    break;
 
                 case "добавить покупку":
 
                     System.out.println("введите id товара");
                     int id_pokypki = scanner.nextInt();
-                    if (id_pokypki <= i && id_pokypki > 0 && !deleted_product[id_pokypki]){
-                        sales_counter++;
-                        System.out.println("покупка завершена, был куплен товар с:");
-                        System.out.println("id: " + sales_counter + " названием " + name_tavara[id_pokypki] + " c ценой " + price_tavara[id_pokypki]);
-                        System.out.println("id чека: " + sales_counter);
-                    }else {
+                    if (id_pokypki >= 0) {
+                        if (id_pokypki <= i && !deleted_product_list.get(id_pokypki)) {
+                            sales_counter++;
+                            System.out.println("покупка завершена, был куплен товар с:");
+                            System.out.println("id: " + id_tavara_list.get(id_pokypki) + " названием " + name_tavara_list.get(id_pokypki) + " c ценой " + price_tavara_list.get(id_pokypki));
+                            System.out.println("id чека: " + sales_counter);
+                        } else {
+                            System.out.println("Error: такого id не существует или был удалён");
+                        }
+                    } else {
                         System.out.println("Error: такого id не существует или был удалён");
                     }
 
@@ -64,37 +67,43 @@ public class Main {
                     working = false;
                     break;
                 case "удалить товар":
-                    System.out.println("введите id товара который хотите удалить");
-                    int delete = scanner.nextInt();
-                    if (deletes[delete] < i){
-                        id_tavara = null;
-                        name_tavara = null;
-                        price_tavara = null;
+                    if (i >= 0){
+                        System.out.println("введите id товара который хотите удалить");
+                        int delete = scanner.nextInt();
+                        String cash_data_delite = name_tavara_list.get(delete);
 
-                        deleted_product[delete] = true;
+                        if (delete <= i){
+                            id_tavara_list.set(delete, -1);
+                            name_tavara_list.set(delete, "null");
+                            price_tavara_list.set(delete, -1);
 
-                        System.out.println("товар " + name_tavara[delete] + " был удалём");
-                    }else{
-                        System.out.println("товар не найдей");
+                            deleted_product_list.set(delete, true);
+
+                            System.out.println("товар " + cash_data_delite + " был удалён");
+                        }else{
+                            System.out.println("товар не найден");
+                        }
+                    }else {
+                        System.out.println("нету товара");
                     }
 
                     scanner.nextLine();
                     break;
 
                 case "список товара":
-                    if (i > 0){
+                    if (i > -1){
                         for (int i1 = 0; i1 <= i; i1++){
-                            System.out.println("id: " + id_tavara[i1] + " название: " + name_tavara[i1] + " цена: " + price_tavara[i1]);
+                            System.out.println("id: " + id_tavara_list.get(i1) + " названием: " + name_tavara_list.get(i1) + " цена: " + price_tavara_list.get(i1));
+
                         }
                     }else {
-                        System.out.println("товар не найден");
+                        System.out.println("нету товара");
                     }
+
                     break;
-                    /*
                 case "help":
                     comands.help();
                     break;
-                     */
                 default:
                     System.out.println("Error: неизвестная команда");
                     break;
